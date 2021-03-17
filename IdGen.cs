575e8e9fafbb20c2006eecdc4dca8b3fa283c528
Gabriel <75862004+Gabriel1961@users.noreply.github.com>
2021-03-17 16:11:59 +0200

    Repar Server

---
+++ IdGen.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace WebSocket___Server
+{
+    public static class IdGen
+    {
+        public static Dictionary<int, bool> m_buffer = new Dictionary<int, bool>(1000);
+        public static int New()
+        {
+            Random rnd = new Random(DateTime.Now.Second);
+            int x;
+            do
+            {
+            x = rnd.Next(1000000, 2000000);
+            } while (m_buffer.ContainsKey(x));
+            m_buffer.Add(x, false);
+            return x;
+        }
+    }
+}
